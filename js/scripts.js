04f352e923bc63fdd9243bb7c0e5045b2a5532a5
George Wayne Mwangi <waynegeorge193.com>
2020-07-19 15:49:52 +0300

    Made changes to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,6 @@ function getDayValue (){
   d=(((century/4)-2*century-1)+((5*year/4))+((26*(month+1)/10))+day)%7;
   console.log(d);
   return (Math.floor(d));
-  if (d < 0) {
-      d = d * -1;
-    }
-    else if (d > 0) {
-      return d;
-    }
 }
 function getGender(){
   var gender = document.getElementsByName("gender");
@@ -30,50 +24,50 @@ function getGender(){
   switch(gender){
     case gender="male":
       switch(dayValue){
-        case(1):
+        case(0):
         document.getElementById("Results").innerHTML="You were born on " +dayNames[0] + " and Your akan name is " +maleNames[0];
         break;
-        case(2):
+        case(1):
         document.getElementById("Results").innerHTML="You were born on "+dayNames[1] + " and Your akan name is " +maleNames[1];
         break;
-        case (3):
+        case (2):
         document.getElementById("Results").innerHTML="You were born on " +dayNames[2]+ " and Your akan name is " +maleNames[2];
         break;
-        case(4):
+        case(3):
         document.getElementById("Results").innerHTML="You were born on "+dayNames[3] +  " and Your akan name is " +maleNames[3];
         break;
-        case(5):
+        case(4):
         document.getElementById("Results").innerHTML="You were born on "+dayNames[4] +  " and Your akan name is " +maleNames[4];
         break;
-        case(6):
+        case(5):
         document.getElementById("Results").innerHTML="You were born on "+dayNames[5] +  " and Your akan name is " +maleNames[5];
         break;
-        case(0):
+        case(6):
         document.getElementById("Results").innerHTML="You were born on "+dayNames[6] + " and Your akan name is " +maleNames[6];
         break;
       }
       break;
       case gender= "female":
       switch (dayValue ){
-        case(1):
+        case(0):
         document.getElementById("Results").innerHTML="You were born on "+dayNames[0] + " and Your akan name is  " +femaleNames[0];
         break;
-        case( 2):
+        case(1):
         document.getElementById("Results").innerHTML="You were born on " +dayNames[1] + " and Your akan name is " +femaleNames[1];
         break;
-        case( 3):
+        case(2):
         document.getElementById("Results").innerHTML="You were born on " +dayNames[2] + " and Your akan name is " +femaleNames[2];
         break;
-        case(4):
+        case(3):
         document.getElementById("Results").innerHTML="You were born on " +dayNames[3] + " and Your akan name is " +femaleNames[3];
         break;
-        case(5):
+        case(4):
         document.getElementById("Results").innerHTML="You were born on " +dayNames[4] + " and Your akan name is " +femaleNames[4];
         break;
-        case(6):
+        case(5):
         document.getElementById("Results").innerHTML="You were born on " +dayNames[5] + " and Your akan name is " + femaleNames[5];
         break;
-        case(-0):
+        case(6):
         document.getElementById("Results").innerHTMl="You were born on " +dayNames[6] + " and Your akan name is " +femaleNames[6];
         break;
       }
